c572f70a54fc834e966dd5f29bc5698c3d3a3b2f
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2025-02-04 12:43:14 +0530

    delete

---
+++ frontend/src/components/Auth/SignupForm.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function SignupForm() {
+  return (
+    <div>SignupForm</div>
+  )
+}
+
+export default SignupForm
---
+++ frontend/src/components/Dashboard/CVUploadForm.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function CVUploadForm() {
+  return (
+    <div>CVUploadForm</div>
+  )
+}
+
+export default CVUploadForm
