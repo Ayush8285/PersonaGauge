b661d5f72687ac220355f24c7b921c397f107989
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2025-02-05 22:31:55 +0530

    mongoDB connect

---
+++ backend/personaGauge/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import user_collection
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1> Hello World app is working </h1>")
+
+
+
+def add_user(request):
+    records={
+        'name':'John Doe',
+        'country':'USA',    
+    }
+
+    user_collection.insert_one(records)
+    return HttpResponse("<h1> user addd </h1>")
+
+
