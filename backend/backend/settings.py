9f74e3507f21dcaf532b01dcc339365c58b69182
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2025-08-11 11:58:59 +0530

    ok

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-gm#(pr)_(lfb!-0uv=$b6-gw$lyt##m!gysk@r5^3r#zi&z7vn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['personagauge.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['personagauge-1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
